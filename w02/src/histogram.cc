bbe8d00356bc3175a2ab9fedd945a26cfa5c37b4
Manish Shambu <mash3503@colorado.edu>
2017-09-23 19:32:02 -0600

    Histogram and quicksort openmp

---
+++ w02/src/histogram.cc
@@ -0,0 +1,151 @@
+#ifdef _OPENMP
+#include <omp.h>
+#define NOW()(omp_get_wtime())
+#else
+#define NOW() 0
+#define omp_get_thread_num() 0
+#endif
+
+#include <iostream>
+#include <unistd.h>
+#include "histogram.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+double min_val = 9999999999.0;
+double max_val = -1.0;
+int nb = 0;
+double bin_range = 0;
+typedef double mytime_t;
+int num_threads =0;
+
+void findminmax(const double *x, size_t n)
+{
+	#pragma omp parallel for reduction(max:max_val) 
+	for(int i=0; i < n; i++)
+	{
+		if(x[i] > max_val)
+		{
+			max_val = x[i];
+		}
+	}
+
+	#pragma omp parallel for reduction(min: min_val)
+	for(int i=0; i < n; i++)
+	{
+		if(x[i] < min_val)
+		{
+			min_val = x[i];
+		}
+	}
+}
+void histogram(size_t n, const double *x, int num_bins, double **bin_bdry, size_t **bin_count)
+{
+	findminmax(x, n);
+	double* bb = new double[num_bins+1];
+	size_t* bc = new size_t[num_bins];
+	*bin_bdry = bb;
+	*bin_count = bc;
+	
+	// Initialize bin boundaries and bin count to 0
+	for(int i=0; i<num_bins; i++)
+	{
+		bb[i] = 0;
+		bc[i] = 0;
+	}
+	bb[num_bins+1] = 0;
+
+	double binRange = (max_val - min_val)/num_bins;
+	mytime_t start = NOW();
+	#pragma omp parallel
+	{
+		size_t *histogram_private_count = new size_t[num_bins];
+		num_threads = omp_get_num_threads();
+		#pragma omp for
+		for(size_t i =0; i<num_bins; i++)
+		{
+			histogram_private_count[i] = 0;
+			bb[i] = min_val + binRange*i;
+		}
+	
+		#pragma omp single
+		bb[num_bins] = min_val + binRange*num_bins;
+	
+		#pragma omp barrier
+		
+		#pragma omp for
+		for(int i=0; i<=num_bins; i++)
+		{
+			bb[i] = min_val + binRange*i;
+		}
+			
+		#pragma omp barrier 
+		#pragma single nowait
+		{
+			bb[0] -= 1E-14;
+			bb[num_bins] += 1E-14;
+		}
+		
+		#pragma omp barrier
+		#pragma omp for
+	       	for(size_t i=0; i<n; i++)
+		{
+			for(int j = 0; j <= num_bins; j++)
+			{
+				if(x[i] >= bb[j] && x[i] < bb[j+1])
+				{	
+					histogram_private_count[j] += 1;	
+				}
+			}
+		}
+		#pragma omp barrier
+	
+		// Add all the local counts from  the threads to the total count.	
+		#pragma omp critical
+		for(int i=0; i<num_bins; i++)
+		{
+			bc[i] += histogram_private_count[i];
+		}	
+	}
+	mytime_t end = NOW();
+	#ifdef _OPENMP
+        std::cout<<"Elapsed time for histogram function with "<<num_threads<<" threads: "<<end-start<<"s"<<std::endl;
+	 #endif
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 3)
+	{
+		printf("Enter \n1.Number of bins\n2.Number of elements\n");
+		return 0;
+	}
+	
+	nb  = atoi(argv[1]);
+	size_t n = atoi(argv[2]);
+	double minNum = 0.0;
+	double maxNum = 9999999999.0;
+	
+
+	double *x = new double[n];
+	for(size_t i=0; i < n; i++)
+	{
+	   double f = (double)rand() / RAND_MAX;
+	   x[i] = minNum + f * (maxNum - minNum);
+	}		
+	
+	double *bb;
+	size_t *bc;
+	
+	histogram(n, x, nb, &bb, &bc);
+      
+	for(int i=0; i<nb; ++i)
+	 {
+		 printf("[bin %d: %5.2e --%5.2e] %d\n",i, bb[i], bb[i+1], bc[i]);
+	 }
+	
+	//delete [] bb;
+	//delete [] bc;
+	
+	return 0;
+}
---
+++ w02/src/quicksort.cc
@@ -0,0 +1,133 @@
+#include "quicksort.h"
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+
+#ifdef _OPENMP
+#include <omp.h>
+#define NOW()(omp_get_wtime())
+#else
+#define NOW()(0)
+#endif
+typedef double mytime_t;
+long lenArr = 99999;
+
+long partition(long *a, int low, int high)
+{
+	// Create 2 arrays.
+	// All the elements less than pivot go to left array
+	// All elements greater than pivot go to right array
+
+	long *left = new long[high-low];
+	long *right = new long[high-low];
+
+	long i;
+	long j;
+
+	// Consider pivot as the last element in the array
+	long pivot = a[high];
+
+	long left_count =0;
+	long right_count =0;
+
+	// Elements higher than pivot in left array
+	// Elements greater than pivot in right array
+	for(i=low; i<high; i++)
+	{
+		if(a[i] < pivot)
+		{
+			left[left_count++] = a[i];
+		}
+		else
+		{
+			right[right_count++] = a[i];
+		} 
+	}
+
+	// Copy all the values in left array to array a first
+	for(i=0; i < left_count; i++)
+	{
+		a[low + i] = left[i];
+	}
+
+	// Insert pivot in correct position 
+	a[low + left_count] = pivot;
+
+	// Copy all the values in right array to 'a'
+	for(j=0; j < right_count; j++)
+	{
+		a[low + left_count + j + 1] = right[j];
+	}
+
+	// return pivot position
+	return low + left_count;
+}
+
+void parallelQsort(long *x, long low, long high)
+{
+	long pi;
+	
+		if(low < high)
+		{
+			pi = partition(x, low, high);
+
+			#pragma omp task
+			{
+			parallelQsort(x, low, pi-1);
+			}
+			
+			#pragma omp task
+			{
+			parallelQsort(x, pi+1, high);
+			}
+		}
+}
+
+
+void quicksort(const long *x, size_t n, long *y)
+{
+	mytime_t start = NOW(); 
+	int num_threads;
+	#pragma omp parallel
+	{ 
+		num_threads = omp_get_num_threads();
+		#pragma omp single nowait
+		parallelQsort(y, 0, n-1);
+	}
+	mytime_t end = NOW();
+	
+	   /*for(long i=0; i<n; i++)
+	   {
+	   printf("%lu\n", *(y+i));
+	   }*/
+	   
+
+	#ifdef _OPENMP
+	std::cout<<"Elapsed time for quicksort with "<<num_threads<<" threads: "<<end-start<<"s"<<std::endl;
+	#endif
+}
+
+int main()
+{
+	long minNum = 1;
+	long maxNum = lenArr;
+	long maxNumDisplayed = 999999999;
+
+	long *x;
+	long *y;
+	x = new long[lenArr];
+	y = new long[lenArr];
+
+	long i;
+	srand(5);
+	// Generate array elements randomly
+	for(i=0; i < lenArr; i++)
+	{
+		x[i] = minNum+(rand()%maxNum);
+		y[i] = x[i];
+	}
+
+	quicksort(x, lenArr, y);
+	delete [] x;
+	delete [] y;
+}
